ca50b924d3485683ee2b7f4c35944ad98107e07c
keenan <keenanburke@outlook.com>
2023-09-14 18:34:19 -0400

    question 3 done except for RMS

--- Question3.py
+++ Question3.py
@@ -28,6 +28,15 @@ def get_least_sqaures_inv(train_x_matrix):
 
     return least_sqaures_inv
 
+def get_regularized_inv(train_x_matrix, regularizer):
+    
+    comp1 = np.transpose(train_x_matrix) * train_x_matrix
+    comp2 = comp1 + regularizer * np.identity(comp1.shape[0])
+    comp2 = np.linalg.pinv(comp2)
+    regularized_inv = comp2 * np.transpose(train_x_matrix)
+    
+    return regularized_inv
+
 rng = np.random.RandomState(seed=0)
 train_x = np.linspace(0, 1, 10)
 precise_x = np.linspace(0, 1, 100)
@@ -64,33 +73,71 @@ weights = get_least_sqaures_inv(train_x_matrix) * np.transpose(np.matrix(train_y
 regressed_output = train_x_matrix * weights
 
 plt.scatter(train_x, np.array(regressed_output))
-
+4
 polynomial_y = np.polyval(weights[::-1], precise_x)
 plt.plot(precise_x, polynomial_y, label="Degree 4 Polynomial", color="orange")
 plt.show()
 
 print()
-"""
+
 
 # C)
 
 print("C)")
 
-plt.scatter(train_x, train_y)
+for poly_degree in range(0,15):
 
-train_x_matrix = generate_x_train_matrix(train_x, 10)
-
-weights = get_least_sqaures_inv(train_x_matrix) * np.transpose(np.matrix(train_y))
-
-regressed_output = train_x_matrix * weights
-
-plt.scatter(train_x, np.array(regressed_output))
-
-polynomial_y = np.polyval(weights[::-1], precise_x)
-plt.plot(precise_x, polynomial_y, label="Degree 4 Polynomial", color="orange")
-plt.show()
-
+    plt.scatter(train_x, train_y)
+    
+    train_x_matrix = generate_x_train_matrix(train_x, poly_degree)
+    
+    weights = get_least_sqaures_inv(train_x_matrix) * np.transpose(np.matrix(train_y))
+    
+    regressed_output = train_x_matrix * weights
+    
+    plt.scatter(train_x, np.array(regressed_output), color='blue')
+    
+    polynomial_y = np.polyval(weights[::-1], precise_x)
+    plt.plot(precise_x, polynomial_y, label="Degree 4 Polynomial", color="orange")
+    
+    original_points_regressed = np.polyval(weights[::-1], train_x)
+    plt.scatter(train_x, original_points_regressed, color="orange")
+    
+    plt.show()
+    
 print()
+"""
+
+# D)
+
+print("D)")
+
+
+
+
+lambds = np.exp(np.linspace(-50,-1, 50))
+
+for regularizer in lambds:
+
+    train_x_matrix = generate_x_train_matrix(train_x, 10)
+    
+    weights = get_regularized_inv(train_x_matrix, regularizer) * np.transpose(np.matrix(train_y))
+    
+    regressed_output = train_x_matrix * weights
+    
+    plt.scatter(train_x, np.array(regressed_output), color='blue')
+    
+    polynomial_y = np.polyval(weights[::-1], precise_x)
+    plt.plot(precise_x, polynomial_y, label="Degree 4 Polynomial", color="orange")
+    
+    plt.scatter(train_x, train_y, color='blue')
+    
+    original_points_regressed = np.polyval(weights[::-1], train_x)
+    plt.scatter(train_x, original_points_regressed, color="orange")
+    
+    plt.show()
+        
+    print()
 
 
 
