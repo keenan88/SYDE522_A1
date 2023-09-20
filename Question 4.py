a893693f6ad2fec13e20b5c736e70442248e7307
keenan <keenanburke@outlook.com>
2023-09-20 19:10:20 -0400

    rough version done

--- Question 4.py
+++ Question 4.py
@@ -4,6 +4,7 @@ import sklearn.linear_model
 import numpy as np
 from IPython import get_ipython
 import matplotlib.pyplot as plt
+from sklearn import datasets
 
 get_ipython().magic('clear')
 get_ipython().magic('reset -f')
@@ -24,7 +25,7 @@ def get_regularized_inv(train_x_matrix, regularizer):
     
     return regularized_inv
 
-diabetes = sklearn.datasets.load_diabetes()
+diabetes = datasets.load_diabetes()
 
 X_train, X_test, Y_train, Y_test = sklearn.model_selection.train_test_split(
     diabetes.data, diabetes.target, test_size=0.3333, shuffle=True,
@@ -33,6 +34,7 @@ X_train, X_test, Y_train, Y_test = sklearn.model_selection.train_test_split(
 X_validate, X_train, Y_validate, Y_train = sklearn.model_selection.train_test_split(
     X_train, Y_train, test_size=0.5, shuffle=True,
 )
+
 lambds = np.exp(np.linspace(-20,5,50))
 
 # A)
@@ -48,7 +50,6 @@ for regularizer in lambds:
     
     reg.fit(diabetes.data, diabetes.target)
     
-    
     training_predictions = reg.predict(X_train)
     validation_predictions = reg.predict(X_validate)
     
@@ -59,17 +60,31 @@ for regularizer in lambds:
     validation_rmses.append(rms_validation)
 
 plt.xscale('log')
-plt.scatter(lambds, training_rmses)
-plt.scatter(lambds, validation_rmses)
+plt.scatter(lambds, training_rmses, label="Training Vs Fit, RMSE")
+plt.scatter(lambds, validation_rmses, label="Validation Vs Fit, RMSE")
 
+plt.xlabel("Regularizer Value")
+plt.ylabel("RMSE")
+plt.title("RMSE vs Regularizer Value")
 plt.ylim(0, 100)
-
+plt.legend()
 plt.show()
+
+
+
+#B
+
+# Running the code multiple times yields RMSE curves that vary 10-20% along the whole curve.
+# However, the general shape of the curve stays the same from trial to trail.
+# In most trials, the RMSE for both training and testing is only increasing with the regularizer.
+# Overfitting would suggest that the regression has a much lower RMSE with the training data
+# than it does with the testing data, but the RMSE for all regularizer values is not observed
+# To be consistently higher for the training data than it is for the testing data.
+
 """
 
-
-# C)
-print("C) ")
+# C/D)
+print("C/D) ")
 
 
 for poly_deg in range(0, 5):
@@ -79,7 +94,6 @@ for poly_deg in range(0, 5):
     F = np.matrix(sklearn.preprocessing.PolynomialFeatures(degree = poly_deg).fit_transform(diabetes.data))
     
     for regularizer in lambds:
-        print(regularizer)
         regularized_inv = get_regularized_inv(F, regularizer)
         
         weights = regularized_inv * np.transpose(np.matrix(diabetes.target))
@@ -90,13 +104,20 @@ for poly_deg in range(0, 5):
          
         poly_rmses.append(rms_polys)
 
-    plt.xscale('log')
-    plt.scatter(lambds, poly_rmses)
-    
-    plt.ylim(0, 100)
-    
-    plt.show()
 
+    plt.scatter(lambds, poly_rmses, label="Poly Fit Degree " + str(poly_deg))
+    
+plt.ylim(0, 125)
+plt.xscale('log')
+plt.xlabel("Regularizer")
+plt.ylabel("RMSE")
+plt.title("Polynomial Fits' RMSEs against Regularizer Values")    
+plt.legend()
+plt.show()
+
+# My laptop can barely handle polynomials of degree 4, so I'll have to hypothesize
+# That the trend observed from polynomials 1 - 4 will continue onwards to polynomials
+# Of higher degrees. 
 
 
 
